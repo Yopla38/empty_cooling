dd1b9fcf67f6b267b8d80d449f51406169008a34
yopla <yoann.cure@cea.fr>
2025-07-01 17:25:03 +0200

    Add docstring

--- sum.py
+++ sum.py
@@ -1,19 +1,19 @@
 import numpy as np
 import time
-def sum(y):
-res = 0
-for yi in y:
-res += yi4
-return res
 
+def sum(y):
+  """Return the sum of all elements in array `y`."""
+  result = 0
+  for yi in y:
+    result += yi
+  return result
 
 def main():
- y = np.random.rand(100000000)
- start = time.time()
- sum(y)
- end = time.time()
- print(f"Elasped time: {end - start}")
-
+  y = np.random.rand(100000000)
+  start = time.time()
+  sum(y)
+  end = time.time()
+  print(f"Elasped time: {end - start} seconds")
 
 if __name__ == "__main__":
   main()
